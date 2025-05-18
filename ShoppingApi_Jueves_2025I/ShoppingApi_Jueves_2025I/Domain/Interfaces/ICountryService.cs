27d4584aa915a158eb95206deb400f3682e00fb7
David Guisao <davidgd20@gmail.com>
2025-05-18 16:34:14 -0500

    ICountryService Created

---
+++ ShoppingApi_Jueves_2025I/ShoppingApi_Jueves_2025I/Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ShoppingApi_Jueves_2025I.DAL.Entities;
+
+namespace ShoppingApi_Jueves_2025I.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountriesAsync(); //una de las tantas firmas de un metodo
+
+        Task<Country> CreateCountryAsync(Country country);
+
+        Task<Country> GetCountryByIdAsync(Guid id);
+
+        Task<Country> EditCountryAsync(Country country);
+
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
---
+++ ShoppingApi_Jueves_2025I/ShoppingApi_Jueves_2025I/Domain/Services/CountryService.cs
@@ -0,0 +1,6 @@
+﻿namespace ShoppingApi_Jueves_2025I.Domain.Services
+{
+    public class CountryService
+    {
+    }
+}
