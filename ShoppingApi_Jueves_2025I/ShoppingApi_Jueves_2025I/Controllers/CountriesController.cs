57724cc362e2e1bde0b6fe7b8e513e98facf8126
David Guisao <davidgd20@gmail.com>
2025-05-29 14:59:08 -0500

    CountriesController Created

---
+++ ShoppingApi_Jueves_2025I/ShoppingApi_Jueves_2025I/Controllers/CountriesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApi_Jueves_2025I.DAL.Entities;
+using ShoppingApi_Jueves_2025I.Domain.Interfaces;
+
+namespace ShoppingApi_Jueves_2025I.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //Este es el nombre inicial de mi ruta o url
+    public class CountriesController : Controller
+    {
+        private readonly ICountryService _countryService;
+
+        public CountriesController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetAll")]
+
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountriesAsync()
+        {
+            var countries = await _countryService.GetCountriesAsync();
+
+            if (countries == null || !countries.Any()) return NotFound();
+            return Ok(countries);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetById/{id}")]
+
+        public async Task<ActionResult<Country>> GetCountryByIdAsync(Guid Id)
+        {
+            var country = await _countryService.GetCountryByIdAsync(Id);
+
+            if (country == null) return NotFound(); //404
+            return Ok(country); //200
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("Create")]
+
+        public async Task<ActionResult<Country>> CreateCountryAsync(Country country)
+        {
+            try
+            {
+                var newCountry = await _countryService.CreateCountryAsync(country);
+                if (newCountry == null) return NotFound();
+                return Ok(newCountry);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit")]
+        public async Task<ActionResult<Country>> EditCountryAsync(Country country)
+        {
+            try
+            {
+                var editedCountry = await _countryService.EditCountryAsync(country); 
+                if(editedCountry == null) return NotFound();
+                return Ok(editedCountry);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete")]
+        public async Task<ActionResult<Country>> DeleteCountryAsync(Guid id)
+        {
+            if (id == null) return BadRequest();
+
+
+            var deletedCountry = await _countryService.DeleteCountryAsync(id);
+
+            if (deletedCountry == null) return NotFound();
+
+            return Ok(deletedCountry);
+        }
+
+    }
+}
--- ShoppingApi_Jueves_2025I/ShoppingApi_Jueves_2025I/Program.cs
+++ ShoppingApi_Jueves_2025I/ShoppingApi_Jueves_2025I/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ShoppingApi_Jueves_2025I.DAL;
+using ShoppingApi_Jueves_2025I.Domain.Interfaces;
+using ShoppingApi_Jueves_2025I.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 
 //Esta es la linea para configurar la conexion a la BD
 builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICountryService, CountryService>();//Contenedor de dependencias
+
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
